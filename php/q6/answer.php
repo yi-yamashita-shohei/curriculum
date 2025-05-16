60f7645dadbd358fcf0815e614f27b23744442b1
yamashita shohei <yamashita_shohei@y-i-group.co.jp>
2025-05-16 21:14:34 +0900

    [fix]:ja修正

--- php/q6/answer.php
+++ php/q6/answer.php
@@ -1,6 +1,6 @@
 <!-- //課題⑥：連想配列でプロフィールを管理しよう -->
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
