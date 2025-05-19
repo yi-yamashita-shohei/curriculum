07e8c361fac220086fa43683da31ca1e9d668e66
yamashita shohei <yamashita_shohei@y-i-group.co.jp>
2025-05-19 20:16:57 +0900

    [fix]:jaに変更

--- php/q5/answer.php
+++ php/q5/answer.php
@@ -1,6 +1,6 @@
 <!-- //課題⑤：関数を作って処理をまとめよう -->
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
